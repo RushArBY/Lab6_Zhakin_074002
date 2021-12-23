58a2b520801f75ee74fe574183434cacc0831d5d
RushArBY <pavel.zhakin2@mail.ru>
2021-12-24 02:42:53 +0300

    Initial commit

---
+++ src/Thelastlab/User.java
@@ -0,0 +1,1 @@
+package Thelastlab; public class User { public static void main(String[] args) { Reader store = new Reader("Регистрация поступлений в продуктовый магазин"); store.setVisible(true); store.setResizable(false); store.setLocationRelativeTo(null); } }

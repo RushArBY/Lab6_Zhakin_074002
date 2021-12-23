cada4622b59b483188b74a82d8901684b90bc25b
RushArBY <pavel.zhakin2@mail.ru>
2021-12-24 02:51:55 +0300

    Init

--- src/Thelastlab/User.java
+++ src/Thelastlab/User.java
@@ -1,1 +1,10 @@
-package Thelastlab; public class User { public static void main(String[] args) { Reader store = new Reader("Регистрация поступлений в продуктовый магазин"); store.setVisible(true); store.setResizable(false); store.setLocationRelativeTo(null); } }
+package Thelastlab;
+
+public class User {
+    public static void main(String[] args) {
+        Reader store = new Reader("Регистрация поступлений в продуктовый магазин");
+        store.setVisible(true);
+        store.setResizable(false);
+        store.setLocationRelativeTo(null);
+    }
+}
